e8ebb666a513d1aa70f7eb6b872cca2836f96663
dianaatorress <dianaatorress@users.noreply.github.com>
2020-04-23 18:10:24 -0500

    cambios

---
+++ HTML, CSS, JavaScript/funciones.js
@@ -0,0 +1,8 @@
+function colorear() {
+  let etiqueta = document.getElementById("aqui")
+  etiqueta.classList.add("letrasMoradas")
+}
+
+function subrayar() {
+  let etiqueta = document.getElementById("aqui")
+}
