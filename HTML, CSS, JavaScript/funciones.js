af0361bb7db1c04fdef83c89193541566531618d
dianaatorress <dianaatorress@users.noreply.github.com>
2020-04-28 17:57:06 -0500

    cambios

--- HTML, CSS, JavaScript/funciones.js
+++ HTML, CSS, JavaScript/funciones.js
@@ -6,3 +6,17 @@ function colorear() {
 function subrayar() {
   let etiqueta = document.getElementById("aqui")
 }
+
+function inicio() {
+  console.log("Hola soy don nadie");
+  let numeroAleatorio = Math.floor(Math.random()*10)
+  console.log(numeroAleatorio)
+
+let intento = prompt("Adivina el numero")
+console.log(intento)
+
+if (intento == numeroAleatorio)
+alert ("Felicidades, le atinaste!!!")
+else
+alert ("Lo siento :(")
+}
