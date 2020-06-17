6b8329e3b8e9aac7b6705f23cdbccb3f8e234c52
dianaatorress <dianaatorress@users.noreply.github.com>
2020-06-16 21:46:51 -0500

    el ultimo estiron

--- mypage.js
+++ mypage.js
@@ -24,6 +24,61 @@ function ocultarlo() {
   modal.style.display = "none";
 }
 
+
+// When the user clicks on the button, open the modal
+function mostrare() {
+  let modal = document.getElementById("mimodal");
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultare() {
+  let modal = document.getElementById("mimodal");
+  modal.style.display = "none";
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+function semuestra() {
+  let modal = document.getElementById("mimmodal")
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function seoculta() {
+  let modal = document.getElementById("mimmodal");
+  modal.style.display = "none";
+}
+
+
+// When the user clicks on the button, open the modal
+function mostrari() {
+  let modal = document.getElementById("mymodaal")
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultari() {
+  let modal = document.getElementById("mymodaal")
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
