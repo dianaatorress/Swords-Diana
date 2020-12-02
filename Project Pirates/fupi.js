eb415cfb1aeffbe1bf4cfa23a957ad1cfeb939de
dianaatorress <dianaatorress@users.noreply.github.com>
2020-12-01 20:05:52 -0600

    Empezamos con funciones

--- Project Pirates/fupi.js
+++ Project Pirates/fupi.js
@@ -1,1 +1,13 @@
 console.log("He´s a pirate");
+
+// When the user clicks on the button, open the modal
+function mostrar() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function ocultar() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
